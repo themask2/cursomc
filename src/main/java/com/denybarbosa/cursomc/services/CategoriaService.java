0be13e73f72ad3945571562cb0dfaccef2e7ed7c
Deny Barbosa <deny.stanley.06@gmail.com>
2021-08-22 13:34:30 -0300

    creating service and repository for Categoria class

---
+++ src/main/java/com/denybarbosa/cursomc/services/CategoriaService.java
@@ -0,0 +1,21 @@
+package com.denybarbosa.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.denybarbosa.cursomc.domain.Categoria;
+import com.denybarbosa.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+
+	@Autowired
+	private CategoriaRepository repo;
+	
+	public Categoria buscar(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null);
+	}
+}
